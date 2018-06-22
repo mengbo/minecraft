08a51dced7ac864166ba71872013f1f92b1064b7
Meng Bo <mengbo@lnu.edu.cn>
2018-06-22 19:11:16 +0800

    refactoring.

--- magics.py
+++ magics.py
@@ -1,15 +1,25 @@
 import mcpi.minecraft as minecraft
-import time
 
 username = input("Username: ")
-
+magicword = input("Magicword: ")
 # mc = minecraft.Minecraft.create(address="127.0.0.1")
 mc = minecraft.Minecraft.create(address="127.0.0.1", name=username)
 
-while True:
-    time.sleep(3)
+
+def magic(command):
+    if "go to" in command:
+        go_to(command)
+    elif "where am i" in command:
+        where_am_i()
+
+
+def go_to(command):
+    pos = command.split('go to ', 1)[1].split(' ')
+    mc.player.setPos(pos[0], pos[1], pos[2])
+    where_am_i()
+
+def where_am_i():
     pos = mc.player.getTilePos()
     name = mc.player.name
     place = name + ': ' + str(pos.x) + ' ' + str(pos.y) + ' ' + str(pos.z)
-    print(place)
     mc.postToChat(place)
---
+++ mcpi-magician.py
@@ -0,0 +1,23 @@
+import magics
+import importlib
+import subprocess
+from pathlib import Path
+
+home = str(Path.home())
+file = Path("%s/Library/Application Support/%s/logs/latest.log" %
+        (home, "for Mc-launcher.com"))
+if not file.exists():
+    file = Path("%s/Library/Application Support/%s/logs/latest.log" %
+            (home, "minecraft"))
+
+f = subprocess.Popen(['tail', '-0', '-F', file],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+lead = "[CHAT] <%s> %s" % (magics.username, magics.magicword)
+
+while True:
+    line = str(f.stdout.readline(), 'utf-8').strip()
+    print(line)
+    if lead in line:
+        command = line.split(lead, 1)[1]
+        magics.magic(command)
